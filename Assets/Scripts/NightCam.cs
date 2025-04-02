9e377bf0bef5528c0b195ef95715f464f72607a8
jaider4419 <jaiderboateng8@gmail.com>
2025-04-02 11:37:50 +0100

    💩

--- Assets/Scripts/NightCam.cs
+++ Assets/Scripts/NightCam.cs
@@ -1,25 +1,28 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NightCam : MonoBehaviour
 {
+    //The variables
     public Transform playerCam;
     public int rotateSpeed, clampValue;
     public bool ADKeys, movingLeft, movingRight;
     public int targetFramerate;
 
-    // Start is called before the first frame update
+    //Sets frame rate of game
     void Start()
     {
         Application.targetFrameRate = targetFramerate;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //If ADKeys is == true, you can rotate the camera left and right with the A and D keys. Leave false if you want to use regular FNAF camera style.
         if (ADKeys == true)
         {
+            //When you hold A, the camera will rotate left.
             if (Input.GetKey(KeyCode.A))
             {
                 if (playerCam.localRotation == Quaternion.Euler(0, -clampValue, 0))
@@ -31,6 +34,7 @@ public class NightCam : MonoBehaviour
                     playerCam.Rotate(0, -rotateSpeed, 0);
                 }
             }
+            //When you hold D, the camera will rotate right.
             if (Input.GetKey(KeyCode.D))
             {
                 if (playerCam.localRotation == Quaternion.Euler(0, clampValue, 0))
@@ -43,10 +47,10 @@ public class NightCam : MonoBehaviour
                 }
             }
         }
-
+        //If ADKeys is == false, the normal FNAF camera rotation will be in play.
         if (ADKeys == false)
-
         {
+            //If either movingLeft or movingRight is == true, the camera will rotate left or right.
             if (movingLeft == true)
             {
                 if (playerCam.localRotation == Quaternion.Euler(0, -clampValue, 0))
@@ -56,7 +60,6 @@ public class NightCam : MonoBehaviour
                 else
                 {
                     playerCam.Rotate(0, -rotateSpeed, 0);
-
                 }
             }
             if (movingRight == true)
@@ -87,6 +90,7 @@ public class NightCam : MonoBehaviour
     }
     public void stopRotateRight()
     {
-        movingRight =false;
+        movingRight = false;
     }
 }
+
