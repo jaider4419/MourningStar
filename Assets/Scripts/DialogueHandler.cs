436e11c836356e8239bcdf9d8102b5e069ff3ed3
jaider4419 <jaiderboateng8@gmail.com>
2025-04-28 10:45:28 +0100

    ok

--- Assets/Scripts/DialogueHandler.cs
+++ Assets/Scripts/DialogueHandler.cs
@@ -23,7 +23,21 @@ public class DialogueHandler : MonoBehaviour
         Dialogues[0].SetActive(true);
         Debug.Log("Its active");
 
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(4);
         Dialogues[0].SetActive(false);
+        yield return new WaitForSeconds(2);
+        Dialogues[1].SetActive(true);
+        yield return new WaitForSeconds(2);
+        Dialogues[1].SetActive(false);
+        yield return new WaitForSeconds(2);
+        Dialogues[2].SetActive(true);
+        yield return new WaitForSeconds(5);
+        Dialogues[3].SetActive(true);
+        yield return new WaitForSeconds(3);
+        Dialogues[3].SetActive(false);
+        yield return new WaitForSeconds(3);
+        Dialogues[4].SetActive(true);
+        yield return new WaitForSeconds(3);
+        StopCoroutine(Dialogue());
     }
 }
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialogueUI;
+    public string[] dialogues;
+    private int currentDialogueIndex = 0;
+
+    private void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    public void StartDialogue(string[] dialogueLines)
+    {
+        dialogues = dialogueLines;
+        currentDialogueIndex = 0;
+        ShowDialogue();
+    }
+
+    private void ShowDialogue()
+    {
+        dialogueUI.SetActive(true);
+        dialogueText.text = dialogues[currentDialogueIndex];
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void DisplayNextDialogue()
+    {
+        currentDialogueIndex++;
+        if (currentDialogueIndex < dialogues.Length)
+        {
+            dialogueText.text = dialogues[currentDialogueIndex];
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialogueUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && dialogueUI.activeSelf)
+        {
+            DisplayNextDialogue();
+        }
+    }
+}
