c6dd8ee82815965703e66d5e19800bd605c41b37
jaider4419 <jaiderboateng8@gmail.com>
2025-05-05 19:25:23 +0100

    Revert "ummmm......"
    This reverts commit 294ca72d0b3031b69ece3eaf34f0d1412f5e51a9.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,26 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    [SerializeField] private string newGameScene = "Level1";
-    [SerializeField] private Button loadGameButton;
-
-    void Start() => loadGameButton.interactable = SaveSystem.SaveExists();
-
-    public void NewGame()
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
     {
-        SaveLoadManager.Instance.NewGame();
-        SceneManager.LoadScene(newGameScene);
+        
     }
 
-    public void LoadGame() => SaveLoadManager.Instance.LoadGame();
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
-    public void QuitGame()
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+    public void ExitGame()
     {
         Application.Quit();
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        Debug.Log("Exited.");
     }
 }
